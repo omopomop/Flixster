51203b7d0f116e59922078cd86b6b5865adfa50d
Eric Wang <e9809wong@gmailcom>
2020-01-22 16:05:11 -0800

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by user163652 on 1/22/20.
+//  Copyright © 2020 ericw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
